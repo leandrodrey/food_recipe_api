cb79ff4bcae42d833eae83c66113394db108bc7b
Leandro Rey <leandrodrey@gmail.com>
2024-08-19 15:34:12 -0300

    Add static methods for fetching and rating recipes
    Replaced database operations with JSON file-based operations. Introduced `getAll` method with a search functionality and `setRecipeRating` method to rate and calculate the average score for recipes.

--- models/recipe.js
+++ models/recipe.js
@@ -1,25 +1,40 @@
+import {readJSON, writeJSON} from "../utils/utils.js";
+
+const recipes = readJSON('./recipes.json');
+
 export class RecipeModel {
-    constructor({db}) {
-        this.db = db;
+
+    static async getAll({searchTerm}) {
+        if (searchTerm) {
+            const lowerCaseSearchTerm = searchTerm.toLowerCase();
+            return recipes.filter((recipe) => {
+                const isInName = recipe.name.toLowerCase().includes(lowerCaseSearchTerm);
+                const isInIngredients = recipe.ingredients.some(ingredient =>
+                    ingredient.name.toLowerCase().includes(lowerCaseSearchTerm)
+                );
+                return isInName || isInIngredients;
+            });
+        }
+        return recipes;
     }
 
-    async getAll({genre}) {
-        return this.db.find({genre});
+    static async setRecipeRating({id, rating}) {
+        const recipe = recipes.find((recipe) => recipe.id === id);
+        if (!recipe) {
+            return false;
+        }
+        recipe.ratings.push(rating);
+
+        const total = recipe.ratings.reduce((sum, r) => sum + r, 0);
+        recipe.score = total / recipe.ratings.length;
+
+        try {
+            await writeJSON('./recipes.json', recipes);
+        } catch (error) {
+            console.error('Error al guardar los cambios en el archivo JSON:', error);
+        }
+
+        return recipe;
     }
 
-    async getById(id) {
-        return this.db.findOne({id});
-    }
-
-    async create({input}) {
-        return this.db.create(input);
-    }
-
-    async delete({id}) {
-        return this.db.delete({id});
-    }
-
-    async update({id, input}) {
-        return this.db.update({id, input});
-    }
 }
